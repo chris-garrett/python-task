707786be7f05c5247898d602bbc10aa63f8c2119
Chris Garrett <chris@nesteggs.ca>
2025-04-18 09:35:39 -0600

    example: beef up compose task with pull, nuke and other changes i use in all projects

--- examples/compose/__task__.py
+++ examples/compose/__task__.py
@@ -1,21 +1,26 @@
 import os
 from __tasklib__ import TaskContext, TaskBuilder
 
+module_name = "tiny"
 
-def _prefix(ctx: TaskContext):
-    compose_file = os.path.join(ctx.project_dir, "docker-compose.yml")
-    return f"docker compose -f {compose_file}"
+
+def _prefix(ctx: TaskContext, no_profiles: bool = False):
+    profiles = "--profile all" if not no_profiles else ""
+    compose_file = os.path.relpath(
+        os.path.join(ctx.project_dir, "docker-compose.yml"), os.curdir
+    )
+    return f"docker compose -f {compose_file} {profiles}"
 
 
 def _up(ctx: TaskContext):
     ctx.log.info("Starting docker-compose")
-    ctx.exec(f"{_prefix(ctx)} up -d")
+    ctx.exec(f"{_prefix(ctx)} up -d --remove-orphans")
 
 
 def _down(ctx: TaskContext):
     ctx.log.info("Stopping docker-compose")
-    ctx.exec(f"{_prefix(ctx)} stop")
-    ctx.exec(f"{_prefix(ctx)} rm -f")
+    ctx.exec(f"{_prefix(ctx, no_profiles=True)} stop")
+    ctx.exec(f"{_prefix(ctx, no_profiles=True)} rm -f")
 
 
 def _logs(ctx: TaskContext):
@@ -28,9 +33,33 @@ def _restart(ctx: TaskContext):
     _up(ctx)
 
 
+def _pull(ctx: TaskContext):
+    ctx.log.info("Pulling docker-compose images ")
+    ctx.exec(
+        "docker login ghcr.io -u USERNAME --password-stdin",
+        input=os.getenv("GITHUB_TOKEN"),
+    )
+    ctx.exec(f"{_prefix(ctx)} pull")
+
+
+def _nuke(ctx: TaskContext):
+    ctx.log.info(f"Removing containers/volumes/ for {module_name}")
+    _down(ctx)
+    ret = ctx.exec(
+        f"docker volume ls --filter 'name={module_name}*' --format '{{{{.Name}}}}'",
+        capture=True,
+    )
+    if ret.returncode != 0:
+        ctx.log.error("Failed to list volumes")
+        return
+    for volume in ret.stdout.splitlines():
+        ctx.exec(f"docker volume rm {volume}")
+
+
 def configure(builder: TaskBuilder):
-    module_name = "datalake"
     builder.add_task(module_name, "up", _up)
     builder.add_task(module_name, "down", _down)
     builder.add_task(module_name, "log", _logs)
     builder.add_task(module_name, "restart", _restart)
+    builder.add_task(module_name, "pull", _pull)
+    builder.add_task(module_name, "nuke", _nuke)
